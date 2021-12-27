d80ef3d5472ac723954f9fe0d2dba76913fa9d88
Mitchell Rodríguez <mitrodle@gmail.com>
2021-12-27 01:42:31 -0500

    add: Ticking Clock

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Root from './routes/root';
+// import { StrictMode } from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+// import Root from './routes/root';
+import './index.css';
 
-const rootElement = document.getElementById('root');
+// const rootElement = document.getElementById('root');
+const javascriptContainer = document.getElementById("javascript");
+const reactjsContainer = document.getElementById("reactjs");
 
-ReactDOM.render(
-  <StrictMode>
-    <BrowserRouter>
-      <Root />
-    </BrowserRouter>
-  </StrictMode>,
-  rootElement
-);
+const render = () => {
+
+    javascriptContainer.innerHTML = `
+        <div class="tick">
+            <h1>Time in Javascript</h1>
+            The time is:
+            <span>${new Date().toLocaleTimeString("en-US")}</span>
+        </div>
+    `;
+
+    ReactDOM.render(
+        //   <StrictMode>
+        //     <BrowserRouter>
+        //       <Root />
+        //     </BrowserRouter>
+        //   </StrictMode>,
+          React.createElement(
+              "div",
+              { className: "tick" },
+              React.createElement(
+                  "h1",
+                  null,
+                  "Time in React"
+              ),
+              "The time is:",
+              React.createElement(
+                  "span",
+                  null,
+                  new Date().toLocaleTimeString("en-US")
+              )
+          ),
+          reactjsContainer,
+        //   rootElement
+    );
+}
+
+setInterval(render, 1000);
+
