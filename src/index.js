f9d7733af3fd6380374debeea9d72f570ab5242c
Mitchell Rodríguez <mitrodle@gmail.com>
2021-12-27 02:11:03 -0500

    add: Hello Dojo

--- src/index.js
+++ src/index.js
@@ -5,45 +5,86 @@ import ReactDOM from 'react-dom';
 // import Root from './routes/root';
 import './index.css';
 
-// const rootElement = document.getElementById('root');
-const javascriptContainer = document.getElementById("javascript");
-const reactjsContainer = document.getElementById("reactjs");
+const rootElement = document.getElementById('root');
+// const javascriptContainer = document.getElementById("javascript");
+// const reactjsContainer = document.getElementById("reactjs");
 
-const render = () => {
+// const render = () => {
 
-    javascriptContainer.innerHTML = `
-        <div class="tick">
-            <h1>Time in Javascript</h1>
-            The time is:
-            <span>${new Date().toLocaleTimeString("en-US")}</span>
-        </div>
-    `;
+//     // javascriptContainer.innerHTML = `
+//     //     <div class="tick">
+//     //         <h1>Time in Javascript</h1>
+//     //         The time is:
+//     //         <span>${new Date().toLocaleTimeString("en-US")}</span>
+//     //     </div>
+//     // `;
 
-    ReactDOM.render(
-        //   <StrictMode>
-        //     <BrowserRouter>
-        //       <Root />
-        //     </BrowserRouter>
-        //   </StrictMode>,
-          React.createElement(
-              "div",
-              { className: "tick" },
-              React.createElement(
-                  "h1",
-                  null,
-                  "Time in React"
-              ),
-              "The time is:",
-              React.createElement(
-                  "span",
-                  null,
-                  new Date().toLocaleTimeString("en-US")
-              )
-          ),
-          reactjsContainer,
-        //   rootElement
-    );
-}
+//     ReactDOM.render(
+//         //   <StrictMode>
+//         //     <BrowserRouter>
+//         //       <Root />
+//         //     </BrowserRouter>
+//         //   </StrictMode>,
+//         //   React.createElement(
+//         //       "div",
+//         //       { className: "tick" },
+//         //       React.createElement(
+//         //           "h1",
+//         //           null,
+//         //           "Time in React"
+//         //       ),
+//         //       "The time is:",
+//         //       React.createElement(
+//         //           "span",
+//         //           null,
+//         //           new Date().toLocaleTimeString("en-US")
+//         //       )
+//         //   ),
+//         //   reactjsContainer,
+//         // rootElement
+//     );
+// }
 
-setInterval(render, 1000);
+// setInterval(render, 1000);
 
+ReactDOM.render(
+    React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            "Hello Dojo!",
+        ),
+        React.createElement(
+            "h2",
+            null,
+            "Things I need to do:",
+        ),
+        React.createElement(
+            "ul",
+            null,
+            React.createElement(
+                "li",
+                null,
+                "Learn React",
+            ),
+            React.createElement(
+                "li",
+                null,
+                "Climb Mt. Everest",
+            ),
+            React.createElement(
+                "li",
+                null,
+                "Run a marathon",
+            ),
+            React.createElement(
+                "li",
+                null,
+                "Feed the dogs",
+            ),
+        ),
+    ),
+    rootElement
+)
